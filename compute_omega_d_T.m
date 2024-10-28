30c56ce118c0d0ea7c2aa9bc4e182b8c85be8dfb
Bespi123 <b_antonioesp@hotmail.com>
2024-10-28 12:36:13 -0500

    Update compute omega.

--- compute_omega_d_T.m
+++ compute_omega_d_T.m
@@ -1,17 +1,24 @@
-unction w_d = compute_omega_d_T(entr)
-    a  = entr(1);
-    nu = entr(2);
-    t  = entr(3);
-    % Compute the desired angular velocity vector ω_d^T at a given time t.
+function w_d = compute_omega_d_T(entr)
+    % Function: compute_omega_d_T
+    % This function computes the desired angular velocity vector ω_d^T 
+    % at a given time t based on the specified amplitude and frequency.
     %
     % Parameters:
-    % a  : Amplitude of the angular velocity
-    % nu : Frequency of the angular velocity
-    % t  : Time
+    %   entr - A vector containing the following parameters:
+    %          entr(1) = Amplitude of the angular velocity (a)
+    %          entr(2) = Frequency of the angular velocity (nu)
+    %          entr(3) = Time (t)
     %
     % Returns:
-    % omega_d_T : A 1x3 row vector [sin(nu * t), cos(nu * t), sin(2 * nu * t)]
+    %   w_d   - A 1x3 column vector containing the computed angular velocities:
+    %           [sin(nu * t), cos(nu * t), sin(2 * nu * t)] scaled by the amplitude.
 
-    % Compute each component of the angular velocity
-    w_d = a * [sin(nu * t), cos(nu * t), sin(2 * nu * t)]';
+    % Extract parameters from the input vector
+    a  = entr(1);  % Amplitude of the angular velocity
+    nu = entr(2);  % Frequency of the angular velocity
+    t  = entr(3);  % Time
+
+    % Compute each component of the angular velocity vector
+    % The resulting vector is scaled by the amplitude 'a' and transposed to a column vector.
+    w_d = a * [sin(nu * t), cos(nu * t), sin(2 * nu * t)]';  % Compute desired angular velocity vector
 end
